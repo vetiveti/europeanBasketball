b312eabff5bb886b12f9ccdffeae15b84ad9d651
Lukas <naund@gmail.com>
2021-04-09 12:55:55 +0200

    new jsn cotains starters

--- R/data_BBL.R
+++ R/data_BBL.R
@@ -42,7 +42,7 @@ bx_team <- get_boxscore_team(pbp,team_h,team_a)
 # compute boxscore for players:
 bx_players <- data.frame()
 for(i in 1:12) {
-    player <- roster$Player[i+12]
+    player <- roster$Player[i]
     bx_player <- get_boxscore_player(pbp,player)
     bx_players <- bind_rows(bx_players, bx_player)
 }
@@ -54,3 +54,51 @@ for(i in 1:12) {
 #' still missing...
 #' playing time for players
 #' auswechslungen und einwechslungen nicht erfasst
+
+
+
+
+url_starter <- gsub(" ","",paste("http://live.easycredit-bbl.de/data",year,
+                              "/bbl/",game_id$home_id,
+                              "/",game_id$game_id,
+                              ".JSN"))
+
+# Which teams did play?
+json_starter <- get_json(url_starter)
+
+df <- json_starter$statind %>% 
+    as_tibble() %>% 
+    rename(teamcode = V1,
+           spielcode = V2,
+           spieler_nummer = V3,
+           pts = V4,
+           ftm = V5,
+           fta = V6,
+           ft_pct = V7,
+           p2m = V8,
+           p2a = V9,
+           p2_pct = V10,
+           p3m = V11,
+           p3a = V12,
+           p3_pct = V13,
+           pf = V14,
+           trb = V15,
+           ast = V16,
+           blk = V17,
+           stl = V18,
+           tov = V19,
+           index1 = V20,
+           index2 = V21,
+           min_mmss = V22,
+           min_mm = V23,
+           drb = V24,
+           orb = V25,
+           starter_1 = V26,
+           starter_2 = V27,
+           starter_3 = V28,
+           starter_4 = V29) %>% 
+    type_convert()
+sum(df$starter_1)
+sum(df$starter_2)
+sum(df$starter_3)
+sum(df$starter_4)
