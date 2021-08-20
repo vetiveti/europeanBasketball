705cbae1166ae2198a4a8731082872fb8e248179
vetiveti <lukas1cramer@gmail.com>
2021-08-20 18:03:19 +0200

    predictions from 2014-2020

--- R/04_03_02_retrodiction.R
+++ R/04_03_02_retrodiction.R
@@ -14,178 +14,10 @@ require(lmtest)
 require(plm)
 require(openxlsx)
 
-# #******************************************************************************#
-# # Wins Produced:----
-# players <- readRDS("Data/players_each_game.Rds")
-# teams <- readRDS("Data/teams_each_game.Rds") %>% 
-#     rename(game = game_nr) %>% 
-#     mutate(REBTM = fga - (opp_tov + opp_fgm + 0.4720415 * opp_ftm + drb - tov
-#                             - 0.4720415 * fta + orb))
-# 
-# marg_values <- readRDS("Data/estimates/marg_values_wp.Rds")
-# DREBMATE = 0.504    # Berri's number
-# tASTpm <- 0.725     # Berri's number
-# pos_avg <- readRDS("Data/estimates/position_average_wp.Rds")
-# 
-# #debugonce(wp)
-# wp.cv <- wp(players,teams,marg_values,DREBMATE,tASTpm,pos_avg) %>% 
-#     filter(., min_p > 0.0)
-# 
-# #******************************************************************************#
-# # Win Shares:----
-# players <- readRDS("Data/players_each_game.Rds")
-# teams <- readRDS("Data/teams_each_game.Rds") %>% 
-#     rename(game = game_nr)
-# 
-# #source('functions/metrics_functions.R')
-# #debugonce(ws)
-# ws.cv <- ws(players,teams) %>% 
-#     filter(min_p > 0.0) %>% 
-#     mutate(ws_pm = win_shares / min_p) %>% 
-#     dplyr::select(player,team,year,game,min_p,win_shares,ws_pm)
-# 
-# #******************************************************************************#
-# # BPM:----
-# player <- readRDS("Data/players_each_game.Rds") %>% 
-#     rename(min = min_p)
-# 
-# team <- readRDS("Data/teams_each_game.Rds") %>% 
-#     rename(game = game_nr)
-# games_total <- team %>%              # Count rows by group
-#     group_by(team,year) %>% 
-#     summarise(G_tot = n(), .groups ="drop")
-# team <- merge(team,games_total,
-#               by = c("team","year"))
-# 
-# point_differential_pg <- 0.0279743 # from regression
-# pos_role <- readRDS("Data/estimates/pos_role_BPM.Rds")
-# 
-# #debugonce(BPM)
-# bpm.cv <- BPM(player,team,pos_role) %>% 
-#     filter(min_p > 0.0) %>% 
-#     mutate(w_add_BPM = VORP * point_differential_pg * G_tot,
-#            w_add_BPM_pm = w_add_BPM / min_p) %>% 
-#     dplyr::select(player,team,year,game,min_p,w_add_BPM,w_add_BPM_pm)
-# 
-# #******************************************************************************#
-# control <- wp.cv %>% 
-#     dplyr::select(player,team,year,game,min_p,wp_total) %>% 
-#     filter(team == "FC Bayern München" & year == 2014) %>% 
-#     group_by(player,team, year) %>% 
-#     summarise_at(vars(wp_total), .funs = sum) %>% 
-#     ungroup()
-# sum(control$wp_total)
-# 
-# #******************************************************************************#
-# # stats1 <- merge(bpm.cv,wp.cv,
-# #                by = c("player","team","year","game","min_p"), all = TRUE)
-# # stats2 <- merge(stats1,ws.cv,
-# #                 by = c("player","team","year","game","min_p"), all = TRUE) %>% 
-# #     filter(.,team == "FC Bayern München" & year == 2014) %>% 
-# #     arrange(game)
-# # 
-# # team_totals <- readRDS("Data/team_data_totals.Rds")
-# # win_true <- team_totals %>% 
-# #     select(team,year,G,W,L)
-# # 
-# # p_2014_fcb <- player %>% 
-# #     filter(.,team == "FC Bayern München" & year == 2014) %>% 
-# #     arrange(game)
-# # unique(p_2014_fcb$game)
-# # 
-# # t_2014_fcb <- team %>% 
-# #     filter(.,team == "FC Bayern München" & year == 2014)
-# # 
-# # # split at 10 games of 30 to predict the remaining 20
-# # first10_p_2014_fcb <- filter(stats2, game <=17224) %>% 
-# #     group_by(player,team) %>% 
-# #     summarise_at(vars(ws_pm,w_add_BPM_pm,wp_pm), funs(weighted.mean(.,min_p))) %>% 
-# #         ungroup()
-# # play_time <- filter(stats2, game <=17224) %>% 
-# #     group_by(player,team) %>%
-# #     summarise_at(vars(min_p), .funs = sum) %>% 
-# #     ungroup()
-# # 
-# # first_p_2014_fcb <- merge(first10_p_2014_fcb,play_time,
-# #                           by = c("player","team"))
-# # 
-# # first10_t_2014_fcb <- filter(t_2014_fcb, game <=17224) %>% 
-# #     dplyr::select(team,G,W,L,min) %>% 
-# #     group_by(team) %>% 
-# #     summarise_at(vars(G:min), .funs = sum) %>% 
-# #     ungroup() %>%
-# #     rename(min_t = min)
-# # 
-# # first10_merged <- merge(first_p_2014_fcb,first10_t_2014_fcb,
-# #                         by = "team") %>% 
-# #     mutate(min_pct = min_p /min_t) 
-# # 
-# # sum(first10_merged$min_pct)
-# # 
-# # # last 20 games
-# # last20_t_2014_fcb <- filter(t_2014_fcb, game >17224) %>% 
-# #     dplyr::select(team,G,W,L,min) %>% 
-# #     group_by(team) %>% 
-# #     summarise_at(vars(G:min), .funs = sum) %>% 
-# #     ungroup() %>%
-# #     rename(min_t = min)
-# # 
-# # pred_merge <- merge(first10_merged,last20_t_2014_fcb,
-# #                     by = "team", suffixes = c("_b","_a")) %>% 
-# #     rename(min_to_play = min_t_a)
-# # 
-# # prediction <- pred_merge %>% 
-# #     mutate(min_p_pred = min_pct * min_to_play,
-# #            ws_pred = ws_pm * min_p_pred,
-# #            wp_pred = wp_pm * min_p_pred,
-# #            BPM_pred = w_add_BPM_pm * min_p_pred) %>% 
-# #     dplyr::select(player,team,min_p_pred,ws_pred,wp_pred,BPM_pred,W_a) %>% 
-# #     summarise_at(vars(min_p_pred,ws_pred,wp_pred,BPM_pred,W_a), .funs = sum) %>% 
-# #     mutate(W_a = W_a / 10)
-# 
-# #******************************************************************************#
-# # doing that for all teams at the same time in a given year
-# source('functions/prediction_functions.R')
-# require(Metrics)
-# 
-# team <- readRDS("Data/teams_each_game.Rds") %>% 
-#     rename(game = game_nr)
-# 
-# model1 <- merge(bpm.cv,wp.cv,
-#                 by = c("player","team","year","game","min_p"), all = TRUE)
-# all_models <- merge(model1,ws.cv,
-#                     by = c("player","team","year","game","min_p"), all = TRUE) #%>% 
-#     # mutate(w_add_BPM_pm = ifelse(min_p > 8,w_add_BPM_pm,0.045),
-#     #       wp_pm = ifelse(min_p > 12,wp_pm,0.045),
-#     #       ws_pm = ifelse(min_p > 12,ws_pm,0.045))
-# 
-# frac <- 0.5
-# years <- 2014:2018
-# seed <- 12
-# source('functions/prediction_functions.R')
-# #debugonce(pred_rmse)
-# coolio <- pred_rmse(all_models,player,team,years,frac,seed)
-# 
-# reg1 <- lm(data = coolio,
-#            W_a ~wp_pred)
-# summary(reg1)
-# 
-# reg2 <- lm(data = coolio,
-#            W_a ~ws_pred)
-# summary(reg2)
-# 
-# reg3 <- lm(data = coolio,
-#            W_a ~BPM_pred)
-# summary(reg3)
-# 
-# cor(coolio$W_a,coolio$wp_pred)
-# cor(coolio$W_a,coolio$ws_pred)
-# cor(coolio$W_a,coolio$BPM_pred)
-
 #******************************************************************************#
 # Inputs
 # year input
-year_input <- 2018
+year_input <- 2020
 
 # splitting the season into parts
 fraction <- 0.5
@@ -260,7 +92,8 @@ bpm.cv <- BPM(df_player,df_team,pos_role) %>%
     dplyr::select(player,team,year,min_p,w_add_BPM,w_add_BPM_pm, VORP)
 
 #******************************************************************************#
-# reality check
+#******************************************************************************#
+# reality check:----
 real_wins <- readRDS("Data/teams_each_game.Rds") %>% 
     rename(game = game_nr) %>% 
     filter(., game %in% train_games$game) %>% 
@@ -268,7 +101,7 @@ real_wins <- readRDS("Data/teams_each_game.Rds") %>%
     group_by(team,year) %>% 
     summarise_at(vars(G:L), sum)
 
-print(paste0("mean number of Games for each team in train sample ",mean(real_wins$G)))
+print(paste0("mean number of Games for each team in train sample: ",round(mean(real_wins$G),digits = 2)))
 
 model1 <- merge(bpm.cv,wp.cv,
                 by = c("player","team","year","min_p"), all = TRUE)
@@ -292,7 +125,7 @@ reg3 <- lm(data = win_train,
 summary(reg3)
 
 #******************************************************************************#
-# now the prediction part
+# prediction part:----
 model1 <- merge(bpm.cv,wp.cv,
                 by = c("player","team","year","min_p"), all = TRUE)
 all_models <- merge(model1,ws.cv,
@@ -311,7 +144,7 @@ minutes_to_play <- readRDS("Data/teams_each_game.Rds") %>%
            L_to_go = L,)
 
 prediction_stats <- merge(all_models,minutes_to_play,
-                     by = c("team","year")) %>% 
+                          by = c("team","year")) %>% 
     mutate(min_pred = min_pct * min_to_go,
            ws_pred = ws_pm * min_pred,
            wp_pred = wp_pm * min_pred,
@@ -327,7 +160,7 @@ prediction <- prediction_stats %>%
            BPM_pred2 = BPM_pred + (G_to_go / 2))
 
 #******************************************************************************#
-# Power, Error and correlation
+# R^2, Error and correlation:----
 # MRSE
 require(Metrics)
 error <- prediction %>% 
@@ -387,7 +220,9 @@ ggplot(data = prediction, aes(x = dumbo2, y = W_to_go)) +
 
 #******************************************************************************#
 #******************************************************************************#
-# Retrodiction (minutes to play are known in advance)
+# Retrodiction :----
+# (minutes to play are known in advance)
+
 # minutes per player
 retro_player <- player %>% 
     filter(., !game %in% train_games$game & year == year_input) %>% 
@@ -408,7 +243,7 @@ team_stats_left <- team %>%
 
 # merge with players
 retr_player <- merge(retro_player,team_stats_left,
-                      by = "team", all = TRUE)
+                     by = "team", all = TRUE)
 
 all_player <- merge(retr_player,prediction_stats, by = c("player","team")) %>% 
     rename(player_name = player) %>% 
@@ -453,8 +288,8 @@ retrodiction <- merge(all_models,retr_player,
            VORP = ifelse(VORP == -100, -2, VORP),
            
            BPM_pred = VORP * point_differential_pg * games_left) %>%  #+ (games_left / 2 / players),
-           # w_add_BPM_pm = ifelse(min_p == 0,0, w_add_BPM / min_p),
-           # BPM_pred = w_add_BPM_pm * min_to_go) %>% 
+    # w_add_BPM_pm = ifelse(min_p == 0,0, w_add_BPM / min_p),
+    # BPM_pred = w_add_BPM_pm * min_to_go) %>% 
     group_by(team,games_left,W_to_go) %>% 
     summarise_at(vars(ws_pred,wp_pred,BPM_pred,VORP), .funs = sum) %>% 
     ungroup() %>% 
@@ -462,7 +297,7 @@ retrodiction <- merge(all_models,retr_player,
            BPM_pred2 = BPM_pred + (games_left / 2))
 
 #******************************************************************************#
-# Power, Error and correlation
+# R^2, Error and correlation
 # MRSE
 require(Metrics)
 error_retro <- retrodiction %>% 
@@ -471,7 +306,7 @@ error_retro <- retrodiction %>%
            BPM_RMSE = rmse(W_to_go,BPM_pred),
            dumbo_RMSE = rmse(W_to_go,dumbo),
            BPM2_RMSE = rmse(W_to_go,BPM_pred2))
-           
+
 # R^2
 reg1 <- lm(data = retrodiction,
            W_to_go ~ wp_pred)
@@ -486,13 +321,12 @@ reg3 <- lm(data = retrodiction,
 summary(reg3)
 
 reg3b <- lm(data = retrodiction,
-           W_to_go ~ BPM_pred2)
+            W_to_go ~ BPM_pred2)
 summary(reg3b)
 
 reg4 <- lm(data = retrodiction,
            W_to_go ~dumbo)
 summary(reg4)
-
 
 
 # plots to correlation regression:
@@ -522,12 +356,12 @@ ggplot(data = retrodiction, aes(x = dumbo, y = W_to_go)) +
                 formula = y ~ x)
 
 #******************************************************************************#
-# now converting wins to per-game point differential 
+# per-game point differential :----
 G_tot <- mean(games_total$G_tot[games_total$year == year_input])
 pd_pg <- retrodiction %>% 
     mutate(wp_pd_pg = (wp_pred - (mean(games_left) / 2)) / (point_differential_pg * G_tot),
            ws_pd_pg = (ws_pred - (mean(games_left) / 2)) / (point_differential_pg * G_tot),
-           )
+    )
 
 pd_pg_team <- team_stats_left <- team %>% 
     filter(., !game %in% train_games$game & year == year_input) %>% 
@@ -560,79 +394,9 @@ reg3 <- lm(data = pd_pg_merg,
            pd_pg_true ~ VORP)
 summary(reg3)
 
-
-
-
 #******************************************************************************#
-# KA was CV bringen soll... oder wie das geht...
-# das was ich gemacht habe ist auf jedenfall falsch...
-# ich müsste ihm ja wenigstens die spiele alle geben, sonst ist das ja komplett unnütz
-# trying <- readRDS("Data/estimates/ranking.Rds") %>% 
-#     filter(., year == year_input) %>% 
-#     group_by(team) %>% 
-#     summarise_at(vars(VORP,wp_A,win_shares), sum)
-# 
-# real_wins <- readRDS("Data/teams_each_game.Rds") %>% 
-#     filter(year == year_input) %>% 
-#     dplyr::select(team,G,W,L) %>% 
-#     group_by(team) %>% 
-#     summarise_at(vars(G:L), sum)
-# 
-# merger <- merge(trying,real_wins, by = "team") %>% 
-#     mutate(BPM = VORP * point_differential_pg * G, .keep = "unused")
+# save:----
+saveRDS(object = error_retro, file = paste0("Data/prediction/error_retro_",year_input,".Rds"))
+saveRDS(object = error_pd, file = paste0("Data/prediction/error_pd_",year_input,".Rds"))
 
 
-# library(DAAG)
-# model1.daag<-CVlm(data = merger,m=3,form.lm=formula(W~BPM))
-# model2.daag<-CVlm(data = merger,m=2,form.lm=formula(W~wp_A))
-
-
-
-# Correlation
-cor(prediction$W_to_go,prediction$wp_pred)
-cor(prediction$W_to_go,prediction$ws_pred)
-cor(prediction$W_to_go,prediction$BPM_pred)
-
-#******************************************************************************#
-
-
-
-
-
-
-
-
-
-##################################
-# a <- t(prediction_teams) %>% as_tibble()
-# colnames(a) <- prediction_teams$team
-# a$first <- colnames(prediction_teams)
-# 
-# a <- a %>% 
-#     relocate(first, .before = 1)
-# a <- a[-1,]
-
-# teams <- df_team$team
-# team_current <- teams
-# 
-# junge <- read.xlsx("data/junge.xlsx", rowNames = FALSE, colNames = TRUE)
-# 
-# 
-# rating_team <- junge %>% 
-#     mutate(#marg_of_victory = (pts - opp_pts) / G,
-#         mov = marg_of_victory,
-#         opp_avg = 0) %>% 
-#     dplyr::select(team,marg_of_victory,mov,opp_avg)
-# 
-# for (team_current in junge$team) {
-#     
-#     cur_team <- rating_team %>% 
-#         filter(team == team_current)
-#     
-#     cur_opp_avg <- rating_team %>% 
-#         filter(team != team_current) %>% 
-#         summarise_at(vars(mov), mean)
-#     
-#     rating_team$opp_avg[rating_team$team == team_current] <-  cur_opp_avg$mov
-#     rating_team$mov[rating_team$team == team_current] <- cur_team$marg_of_victory + cur_opp_avg$mov
-# }
